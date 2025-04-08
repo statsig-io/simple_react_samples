d0e91c016648609b5bdbfa525afc3326b4b296f0
Brock Lumbard <brocklumbard@statsigadmins-MacBook-Air.local>
2025-04-07 20:04:26 -0700

    add statsig flag

--- sample-react-flags/src/app/page.tsx
+++ sample-react-flags/src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { StatsigProvider, useFeatureGate, useGateValue, useStatsigClient } from "@statsig/react-bindings";
+import { Component, useState } from "react";
 
-export default function Home() {
+export function MyComponent() {
   const [bananaCount, setBananaCount] = useState(0);
+  const [appleCount, setAppleCount] = useState(0);
   const [showBanana, setShowBanana] = useState(false);
+  const [showApple, setShowApple] = useState(false);
 
   const addToCart = () => {
     setBananaCount(bananaCount + 1);
@@ -12,6 +15,17 @@ export default function Home() {
     setTimeout(() => setShowBanana(false), 500);
   };
 
+  const addAppleToCart = () => {
+    setAppleCount(appleCount + 1);
+    setShowApple(true);
+    setTimeout(() => setShowApple(false), 500);
+  };
+
+  const gate = useFeatureGate('apple_gate');
+  
+  const gateValue = gate.value;
+  console.log("gate");
+  console.log(gate.details);
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8 gap-6 font-serif">
       <h1 className="text-5xl font-bold text-yellow-600">Brock's banana store</h1>
@@ -25,13 +39,43 @@ export default function Home() {
         >
           Add banana to cart
         </button>
+        {gateValue && (
+          <button 
+            onClick={addAppleToCart}
+            className="bg-red-400 px-4 py-2 rounded hover:bg-red-500 transition-colors text-black"
+        >
+          Add apple to cart
+        </button>
+        )}
         <div className="flex items-center">
           <span>bananas in cart: {bananaCount}</span>
           {showBanana && (
             <span className="ml-2 animate-bounce">🍌</span>
           )}
         </div>
+        {gateValue && (
+          <div className="flex items-center">
+            <span>apples in cart: {appleCount}</span>
+            {showApple && (
+              <span className="ml-2 animate-bounce">🍎</span>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
 }
+
+export default function Home() {
+  return (
+    <>
+    <StatsigProvider sdkKey="client-yr1tFsoqiCicrhc8p4Ylbn9oDjAD2ToPtBtDijsWSKQ" user={{}}
+    loadingComponent={
+        <div style={{ height: 100, width: 300, padding: 16 }}>Loading...</div>
+      }
+    >
+      <MyComponent />
+    </StatsigProvider>
+    </>
+  );
+};
